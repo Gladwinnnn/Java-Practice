7323b3250e961f694639d5bd9a60ecac63f2ee5b
Gladwinnnn <gladwin1503@hotmail.com>
2020-07-04 18:44:44 +0800

    Initial commit

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add{
+    public static void main(String[] args){
+        int number1 = 5; 
+        int number2 = 10;
+        int totalSum = number1 + number2;
+        int product = number1 * number2;
+
+        System.out.println("The total is " + totalSum);
+        System.out.println("The product is " + product);
+    }
+}
---
+++ Check.java
@@ -0,0 +1,14 @@
+public class Check{
+    public static void main(String[] args){
+        int number = 100;
+        if (number > 0){
+            System.out.println(number + " is a positive number");
+        }
+        else if (number < 0){
+            System.out.println(number + " is a negative number");
+        }
+        else{
+            System.out.println(number + " is neither positive nor negative");
+        }
+    }
+}
