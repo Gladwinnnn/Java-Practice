3f2fccd4844de341035907f24cd32c36ffca42d9
Gladwinnnn <gladwin1503@hotmail.com>
2020-08-12 20:15:40 +0800

    java prac

---
+++ EvenLength.java
@@ -0,0 +1,7 @@
+public class EvenLength{
+    public static void main(String[] args){
+        String testData = "Python";
+        int index = testData.length() / 2;
+        System.out.print(testData.substring(0,index));
+    }
+}
