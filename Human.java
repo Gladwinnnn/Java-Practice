a6cb446444d8585b4edbbc2b2bad1811b58efd8a
Gladwinnnn <gladwin1503@hotmail.com>
2020-08-26 11:51:14 +0800

    practice on classes

---
+++ Human.java
@@ -0,0 +1,18 @@
+public class Human{
+    private String name;
+    private double weight;
+    private double height;
+
+    // constructor (something like a method)
+    public Human(){
+        name = "nameless";
+        weight = 0;
+        height = 0;
+    }
+
+    public Human(String n, double w, double h){
+        name = n;
+        weight = w;
+        height = h;
+    }
+}
